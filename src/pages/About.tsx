cc975acba7c27d492a42f1a1fa1e2c0601465a32
DennisNevback <dennis.nevback@gmail.com>
2025-07-01 10:32:18 +0200

    Added images and styling for Home About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,11 +2,14 @@
 function About() {
 
   return (
-    <div className="flex flex-col justify-start pb-5 space-y-3 wrap-normal px-3 sm:pl-3 md:pl-15 xl:px-50 2xl:px-170">
-      <h2 className="text-2xl">Om Dennis Dessert</h2>
-      <p>Dennis Dessert föddes ur en enkel idé: att göra vardagen lite sötare. Grundaren, Dennis, har alltid haft en passion för både teknik och bakning – två världar som sällan möts, men som i hans kök smälte samman på ett oväntat sätt.</p>
-      <p>Allt började med en hemmagjord chokladfondant och ett enkelt beställningsformulär online. Det som började som ett sidoprojekt växte snabbt till ett företag med ett tydligt mål: att sprida glädje genom handgjorda desserter – skapade med precision, passion och en nypa kreativitet.</p>
-      <p>På Dennis Dessert tror vi på att göra det lilla extra – från noga utvalda ingredienser till presentationen i varje leverans. Vårt sortiment växer ständigt, men vårt fokus förblir detsamma: kvalitet, hantverk och personlig service.</p>
+    <div className="flex flex-col xl:flex-row justify-start items-center wrap-normal px-3 mb-5 sm:px-3 md:px-15 xl:px-50 2xl:px-170">
+      <div className="flex flex-col space-y-3 pb-5 xl:pr-20">
+        <h2 className="text-2xl">Om Dennis Dessert:</h2>
+        <p>Dennis Dessert föddes ur en enkel idé: att göra vardagen lite sötare. Grundaren, Dennis, har alltid haft en passion för både teknik och bakning – två världar som sällan möts, men som i hans kök smälte samman på ett oväntat sätt.</p>
+        <p>Allt började med en hemmagjord chokladfondant och ett enkelt beställningsformulär online. Det som började som ett sidoprojekt växte snabbt till ett företag med ett tydligt mål: att sprida glädje genom handgjorda desserter – skapade med precision, passion och en nypa kreativitet.</p>
+        <p>På Dennis Dessert tror vi på att göra det lilla extra – från noga utvalda ingredienser till presentationen i varje leverans. Vårt sortiment växer ständigt, men vårt fokus förblir detsamma: kvalitet, hantverk och personlig service.</p>
+      </div>
+      <img src="./images/image/anete-t-2eEPCNEE3h8-unsplash.jpg" alt="" className="md:w-100 xl:h-100 rounded-sm" />
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,15 +3,15 @@ function Home() {
 
   return (
     <>
-      <div className="flex flex-col justify-start pb-5 space-y-3 wrap-normal px-3 sm:px-3 md:px-12 xl:px-50 2xl:px-170 xl:mt-10">
+      <div className="flex flex-col justify-start pb-5 space-y-3 wrap-normal px-3 sm:px-3 md:px-12 xl:px-60 xl:mt-15 2xl:px-170 ">
         <div>
           <h2 className="text-2xl">Hej!</h2>
           <p>Välkommen till Dennis Dessert. Vi erbjuder muffins och tårtor för all typer av event och fest. </p>
           <p>Kika runt på sidan och tveka inte att kontakta mig via email eller telefon om du har frågot och funderingar om antingen allergier eller beställning</p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-center space-y-5 gap-x-4 xl:mt-3">
-          <img src="./images/cakes/christiann-koepke-AigxB1zfRVo-unsplash.jpg" alt="" />
-          <img src="./images/cakes/james-coleman-5HR1gItc7Gs-unsplash.jpg" alt="" />
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-center space-y-5 gap-x-4 xl:mt-8 xl:px-10">
+          <img src="./images/cakes/christiann-koepke-AigxB1zfRVo-unsplash.jpg" alt="" className="rounded-sm" />
+          <img src="./images/cakes/james-coleman-5HR1gItc7Gs-unsplash.jpg" alt="" className="rounded-sm" />
         </div>
       </div>
     </>
